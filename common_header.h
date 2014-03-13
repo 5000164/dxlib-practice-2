96becadb68cfb02e5d8a6f26f51c76cfab7a5a79
Hiroshi Sugawara <5000164@users.noreply.github.com>
2014-03-14 01:00:11 +0900

    first commit

---
+++ common_header.h
@@ -0,0 +1,4 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
+void MainProcess();
---
+++ main_process.cc
@@ -0,0 +1,14 @@
+#include "common_header.h"
+
+// メイン処理
+void MainProcess()
+{
+	///
+	/// 終了処理
+	///
+
+	// 何らかのキー入力で終了
+	WaitKey();
+
+	return;
+}
